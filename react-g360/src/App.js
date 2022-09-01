6b40a7370a5c6fd2efdb31c4c1807196f4f2c24a
ElisaCarballo <elisa.carballo79@gmail.com>
2022-08-31 22:41:15 -0500

    Proyect_react

--- react-g360/src/App.js
+++ react-g360/src/App.js
@@ -1,19 +1,26 @@
 import "./App.css";
 import Home from "./component/Home";
 import Navbar from "./component/Navbar";
-import {Switch, Route} from "react-router-dom";
 import Products from "./component/Products";
 import Product from "./component/Product";
+
+import {
+  BrowserRouter as Router, 
+  Routes, 
+  Route,  
+} from "react-router-dom";
 
 function App() {
   return (
     <>
     <Navbar/>
-    <Switch>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/products" component={Products} />
-    <Route exact path="/products/:id" component={Product} />
-    </Switch>
+    <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}  />
+          <Route path="/products" element={<Products/>} />
+          <Route path="/products/:id" element={<Product/>} />
+        </Routes>
+    </Router>
     </>
   );
 }
--- react-g360/src/index.js
+++ react-g360/src/index.js
@@ -5,17 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
-import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux'
 import store from './redux/reducer/store'
 
 
 ReactDOM.render(
-  <BrowserRouter>
   <Provider store={store}>
     <App />
-    </Provider>
-  </BrowserRouter>,
+    </Provider>,
   document.getElementById('root')
 );
 
